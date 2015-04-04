764258853bd6cbe200b8c9a0763b2b54efa3deeb
Francis Pieraut <fpieraut@gmail.com>
2015-04-04 07:32:39 +0000

    add beginning padding

--- util/filter.py
+++ util/filter.py
@@ -11,8 +11,13 @@ def movingaverage(interval, window_size=14, pad=True):
         x = np.array(interval)
         n = len(ma)
         start = n-w
-
+        # padding end
         for i in range(start, start+w):
             seq=x[i-w:i]
             ma[i]=seq.sum()/len(seq)
+        # padding begining
+        for i in range(w):
+            seq=x[i-w:i]
+            ma[i]=seq.sum()/len(seq)
+
     return ma
